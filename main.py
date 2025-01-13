723f07180a3d77c5b3ff51e522c0a56bc0d58e95
Aytekin97 <aytecha.97@gmail.com>
2025-01-12 20:41:10 -0500

    Initialize Repository

---
+++ main.py
@@ -0,0 +1,10 @@
+""" 
+    Implement logic here
+
+    1) Schedule jobs to run
+    2) Decide which companies will be sent
+       in the request
+    3) Decide how often will it run
+    4) Create an API that admins can 
+       interct to configure each job
+ """
